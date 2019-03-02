d6de5fa310122266386d40069dbeff0b3364e5e0
Philip Nelson <PhilipNelson5@gmail.com>
2019-03-02 13:57:12 -0700

    added mkdirs for setup script

--- setupScripts/nvim.sh
+++ setupScripts/nvim.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 mkdir -p ~/.config/nvim/config
+mkdir -p ~/.config/nvim/backup
+mkdir -p ~/.config/nvim/swp
+mkdir -p ~/.config/nvim/undo
 ln -sif $(pwd)/../.config/nvim/init.vim ~/.config/nvim/init.vim
 ln -sif $(pwd)/../.config/nvim/config/general.vimrc ~/.config/nvim/config/general.vimrc
 ln -sif $(pwd)/../.config/nvim/config/keys.vimrc ~/.config/nvim/config/keys.vimrc
